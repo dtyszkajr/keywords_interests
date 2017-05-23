25f2a7e2cb31782a270e975ca3da4133f4b12a2c
Tyszka <daniel@hariken.co>
2017-05-23 15:47:07 -0300

    added commerces general scrapper

--- es_index_creator.sh
+++ es_index_creator.sh
@@ -61,3 +61,34 @@ curl -XPUT 'http://localhost:9200/i_rawhtml_v1' -d '
         }
     }
 }'
+
+curl -XPUT 'http://localhost:9200/i_commerceshtml_v1' -d '
+{
+    "settings": {
+        "index": {
+            "refresh_interval": "15s",
+            "number_of_shards": "1",
+            "number_of_replicas": "0"
+        }
+    },
+    "mappings": {
+        "m_commerceshtml_v1": {
+            "properties": {
+                "link": {
+                    "type": "string",
+                    "index": "not_analyzed"
+                },
+                "domain": {
+                    "type": "string",
+                    "index": "not_analyzed"
+                },
+                "raw_html": {
+                    "type": "string",
+                    "doc_values": false,
+                    "index": "no"
+                }
+            }
+        }
+    }
+}'
+
---
+++ scrapper_commerces.py
@@ -0,0 +1,82 @@
+""" Crawls folha for article old pages """
+
+# starting from the home page, this script crawls every link
+# looking for more lings in the target categories and date (2017-04)
+# saving all of them in the local elasticsearch database
+
+# executing this script using
+# nohup python3 -u scrapper_news_links_folha.py > log-scrapper_news_links_folha.log &
+
+from bs4 import BeautifulSoup
+import datetime
+import elasticsearch
+import hashlib
+import requests
+import sys
+import urllib
+
+
+def print_now(text):
+    "prints and flushes the output"  # sometimes the pint function has delays, this solves that problem
+    sys.stdout.write(str(text))
+    sys.stdout.write('\n')
+    sys.stdout.flush()
+
+
+# initial_link = "http://www.ricardoeletro.com.br/"
+initial_link = sys.argv[1]
+# setting string to search in domains
+domain = urllib.parse.urlsplit(initial_link).netloc.replace('www.', '').split('.')[0]
+# initializing url's list to crawl
+all_links = [initial_link]
+# connecting to elasticsearch
+es = elasticsearch.Elasticsearch()
+# initializing list index
+indice = 0
+# loop
+while True:
+    # checking if there is a new link to crawl
+    if indice >= len(all_links):
+        break
+    # getting target url
+    target_url = all_links[indice]
+    # requesting target url
+    page = requests.get(target_url)
+    # parsing page
+    soup = BeautifulSoup(page.text, 'html.parser')
+    # generating empty list
+    new_urls = []
+    # getting all links from page
+    for anchor in soup.find_all('a'):
+        # getting link
+        link = anchor.get('href')
+        # parsing urls
+        url = urllib.parse.urlsplit(link)
+        # filtering only ones with domain www1.folha.uol.com.br
+        if type(link) == str and url.netloc.find(domain) >= 0:
+            # filtering only links in target categories
+            new_urls.append(urllib.parse.urlunsplit((url.scheme, url.netloc, url.path, None, None)))
+    # removing duplicates
+    new_urls = list(set(new_urls))
+    # appending to list of links
+    for url in new_urls:
+        if url not in all_links:
+            all_links.append(url)
+    # raw html content (stripping white space)
+    page_raw_html = ' '.join(page.text.split())
+    # creating link's raw content id
+    link_id = hashlib.sha1(target_url.encode('utf-8')).hexdigest()
+    # storing html content
+    res = es.index(index="i_commerceshtml_v1",
+                    doc_type='m_commerceshtml_v1',
+                    id=link_id,
+                    timeout='120s',
+                    body={
+                        'link': target_url,
+                        'domain': domain,
+                        'raw_html': page_raw_html})
+    if res['created']:
+        print_now('{} scrapped link {}'.format(datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S'),
+                                                target_url))
+    # incrementing the counter
+    indice += 1
